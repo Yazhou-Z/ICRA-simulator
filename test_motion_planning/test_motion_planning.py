707c5610faf8e2681013286009fda43dfa678cd9
Yazhou-Z <76484768+Yazhou-Z@users.noreply.github.com>
2021-01-11 19:50:02 +0800

    Update test_motion_planning.py

--- test_motion_planning/test_motion_planning.py
+++ test_motion_planning/test_motion_planning.py
@@ -1,5 +1,5 @@
 from motion import kernal 
 
-test_game = kernal(car_num = 4, render = True, record = False)
+test_game = kernal(car_num = 4, render = True, record = False) # car_num could be changed accordingly
 test_game.reset()
 test_game.play()
