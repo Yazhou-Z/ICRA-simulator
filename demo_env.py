8858a8b2b16b612fa5a555536c0c902844e488ea
Yazhou-Z <76484768+Yazhou-Z@users.noreply.github.com>
2021-01-13 18:23:00 +0800

    Update demo_env.py

--- demo_env.py
+++ demo_env.py
@@ -1,18 +1,19 @@
-from kernel import kernal
-env = kernal (car_num = 4, render = True, record=False)
-env.reset()
-a = env.orders
+from kernel_for_RL import kernal
+import random
+env = kernal(car_num = 1, robot_id = 1, render = True, record = False)
 def demo():
     total_reward = 0
+    state = env.reset()
     steps = 0
     while True:
-        reward, done = env.eachstep()
+        env.get_order()
+        action = env.orders
+        state, reward, done, info = env.step(action)
+        steps += 1
         total_reward += reward
         if steps % 20 == 0 or done:
            print("step {} total_reward {:+0.2f}".format(steps, total_reward))
-        steps += 1
-        if done: 
-            return total_reward
-        
-if __name__ == '__main__':
-    demo()  
+        if done:
+           return total_reward
+if __name__ =="__main__":
+    demo()
