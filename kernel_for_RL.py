71c6a686788e8477a55c2b2924548a1bffab2176
Yazhou-Z <76484768+Yazhou-Z@users.noreply.github.com>
2021-01-15 19:40:14 +0800

    updated map

--- kernel_for_RL.py
+++ kernel_for_RL.py
@@ -212,18 +212,20 @@ class kernal(object): # gym.Env
         self.lidar_angle = 120 / 2
         self.move_discount = 0.6
         # above are params that can be challenged depended on situation
-        self.map_length = 800
-        self.map_width = 500
+        self.map_length = 808
+        self.map_width = 448
         self.theta = np.rad2deg(np.arctan(45/60))
         self.record=record
-        self.areas = np.array([[[580.0, 680.0, 275.0, 375.0],
-                                [350.0, 450.0, 0.0, 100.0],
-                                [700.0, 800.0, 400.0, 500.0],
-                                [0.0, 100.0, 400.0, 500.0]],
-                               [[120.0, 220.0, 125.0, 225.0],
-                                [350.0, 450.0, 400.0, 500.0],
-                                [0.0, 100.0, 0.0, 100.0],
-                                [700.0, 800.0, 0.0, 100.0]]], dtype='float32')
+        self.areas = np.array([[[708.0, 808.0, 348.0, 448.0],
+                                [708.0, 808.0, 0.0, 100.0]],
+                                [[0.0, 100.0, 0.0, 100.0],
+                                [0.0, 100.0, 348.0, 448.0]]], dtype='float32')
+        self.special_area = np.array([[23.0, 77.0, 145.0, 193.0],
+                                [731.0, 785.0, 255.0, 303.0], 
+                                [377.0, 431.0, 20.5, 25.3], 
+                                [377.0, 431.0, 379.5, 427.5],
+                                [163.0, 217.0, 307.0, 355.0], 
+                                [591.0, 645.0, 141.0, 189.0]], dtype='float32')
         self.barriers = np.array([[350.0, 450.0, 237.5, 262.5],
                                   [120.0, 220.0, 100.0, 125.0],
                                   [580.0, 680.0, 375.0, 400.0],
@@ -242,26 +244,32 @@ class kernal(object): # gym.Env
             self.gray = (180, 180, 180)
             self.red = (190, 20, 20)
             self.blue = (10, 125, 181)
-            # load barriers imgs
+            # load barriers imgs ##
             self.barriers_img = []
             self.barriers_rect = []
             for i in range(self.barriers.shape[0]):
                 self.barriers_img.append(pygame.image.load('./imgs/barrier_{}.png'.format('horizontal' if i < 3 else 'vertical')))
                 self.barriers_rect.append(self.barriers_img[-1].get_rect())
                 self.barriers_rect[-1].center = [self.barriers[i][0:2].mean(), self.barriers[i][2:4].mean()]
-            # load areas imgs
+            # load start imgs OK~
             self.areas_img = []
             self.areas_rect = []
             for oi, o in enumerate(['red', 'blue']):
-                for ti, t in enumerate(['bonus', 'supply', 'start', 'start']):
-                    self.areas_img.append(pygame.image.load('./imgs/area_{}_{}.png'.format(t, o)))
+                for ti, t in enumerate(['start', 'start']):
+                    startpicture = pygame.image.load('./imgs/area_{}_{}.png'.format(t, o))
+                    startpicture = pygame.transform.scale(startpicture, (100,100))
+                    self.areas_img.append(startpicture)
                     self.areas_rect.append(self.areas_img[-1].get_rect())
                     self.areas_rect[-1].center = [self.areas[oi, ti][0:2].mean(), self.areas[oi, ti][2:4].mean()]
-            # load supply head imgs
-            self.head_img = [pygame.image.load('./imgs/area_head_{}.png'.format(i)) for i in ['red', 'blue']]
-            self.head_rect = [self.head_img[i].get_rect() for i in range(len(self.head_img))]
-            self.head_rect[0].center = [self.areas[0, 1][0:2].mean(), self.areas[0, 1][2:4].mean()]
-            self.head_rect[1].center = [self.areas[1, 1][0:2].mean(), self.areas[1, 1][2:4].mean()]
+            # load special_area imgs
+            picture = pygame.image.load('./imgs/special_area.png')
+            picture = pygame.transform.scale(picture, (54,48))            
+            self.special_area_img = [picture for i in range(6)]
+            self.special_area_rect = []
+            for i in range(6):
+                self.special_area_rect.append(self.special_area_img[i].get_rect())
+                self.special_area_rect[-1].center = [self.special_area[i][0:2].mean(), self.special_area[i][2:4].mean()] 
+            # load another imgs
             self.chassis_img = pygame.image.load('./imgs/chassis_g.png')
             self.gimbal_img = pygame.image.load('./imgs/gimbal_g.png')
             self.bullet_img = pygame.image.load('./imgs/bullet_s.png')
@@ -319,7 +327,7 @@ class kernal(object): # gym.Env
         # KO or win reward...
         return reward
 
-    def one_epoch(self):
+    def one_epoch(self):   
         for n in range(self.car_num):
             if not self.epoch % 10:
                 self.orders_to_acts(n)
@@ -477,6 +485,8 @@ class kernal(object): # gym.Env
             self.screen.blit(self.barriers_img[i], self.barriers_rect[i])
         for i in range(len(self.areas_rect)):
             self.screen.blit(self.areas_img[i], self.areas_rect[i]) 
+        for i in range(6):
+            self.screen.blit(self.special_area_img[i], self.special_area_rect[i])
         for i in range(len(self.bullets)):
             self.bullet_rect.center = self.bullets[i].center
             self.screen.blit(self.bullet_img, self.bullet_rect)
@@ -489,8 +499,9 @@ class kernal(object): # gym.Env
             gimbal_rotate_rect.center = self.cars[n, 1:3]
             self.screen.blit(chassis_rotate, chassis_rotate_rect)
             self.screen.blit(gimbal_rotate, gimbal_rotate_rect)
-        self.screen.blit(self.head_img[0], self.head_rect[0])
-        self.screen.blit(self.head_img[1], self.head_rect[1])
+        for i in range(6):
+            self.screen.blit(self.special_area_img[i], self.special_area_rect[i])
+            #self.screen.blit(self.special_area_img[1], self.special_area_rect[1])
         for n in range(self.car_num):
             select = np.where((self.vision[n] == 1))[0]+1
             select2 = np.where((self.detect[n] == 1))[0]+1
@@ -567,7 +578,7 @@ class kernal(object): # gym.Env
         else: self.orders[self.n, 6] = 0
         if pressed[pygame.K_n]: self.orders[self.n, 7] = 1 
         else: self.orders[self.n, 7] = 0
-'''
+        '''
         if pressed[pygame.K_TAB]: self.dev = True
         else: self.dev = False
         return False
